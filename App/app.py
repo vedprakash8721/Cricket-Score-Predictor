5a8a91db47f0590212d2c267571619c5fdeb2682
Ved <askrocks2001@gmail.com>
2025-10-12 17:22:42 +0530

    DI11

--- App/app.py
+++ App/app.py
@@ -16,7 +16,7 @@ cities = ['Hyderabad','Bangalore','Mumbai','Indore','Kolkata','Delhi',
           'Visakhapatnam','Pune','Raipur','Ranchi','Abu Dhabi',
           'Sharjah','Mohali','Bengaluru']
 
-pipe = pickle.load(open("pipe.pkl", "rb"))
+pipe = pickle.load(open("App/pipe.pkl", "rb"))
 
 # ----------------- Page Config -----------------
 st.set_page_config(
