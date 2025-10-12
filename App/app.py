43f5acd19c7586b87e326a2165154a77bf04630f
Ved <askrocks2001@gmail.com>
2025-10-12 12:55:47 +0530

    Web Application is in Process

---
+++ App/app.py
@@ -0,0 +1,9 @@
+""" 
+Streamlit is an open-source Python framework used to build interactive web applications —
+especially for machine learning, data science, and analytics projects —
+without needing any web development skills (like HTML, CSS, or JavaScript).
+
+"""
+
+import streamlit as st
+st.title(" IPL WIN PREDICTION ")
